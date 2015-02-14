ca41f639bed76e05f5910a930b6e28f1589e4b1a
Mcmuffin <rdirective@gmail.com>
2015-02-14 00:56:19 -0600

    Red/Blue nipples,precursor heat exchanger chirality

--- prototypes/entity/entities.lua
+++ prototypes/entity/entities.lua
@@ -567,7 +567,7 @@ data:extend(
       },
       west =
       {
-        filename = "__UraniumPower__/graphics/entity/new-heat-exchanger/heatexchanger1rot.png",
+        filename = "__UraniumPower__/graphics/entity/new-heat-exchanger/heatexchanger1rot-270.png",
 		width = 120,
 		height = 80,
         frame_count = 1,
@@ -575,7 +575,7 @@ data:extend(
       },
       south =
       {
-        filename = "__UraniumPower__/graphics/entity/new-heat-exchanger/heatexchanger1.png",
+        filename = "__UraniumPower__/graphics/entity/new-heat-exchanger/heatexchanger1-180.png",
 		width = 120,
 		height = 80,
         frame_count = 1,
@@ -649,6 +649,150 @@ data:extend(
       {"iron-plate", 1}
     },
     result = "new-heat-exchanger-01"
+  },
+ {
+    type = "assembling-machine",
+    name = "R-new-heat-exchanger-01",
+    icon = "__UraniumPower__/graphics/entity/new-heat-exchanger/Rheatexchanger1icon.png",
+    flags = {"placeable-neutral", "player-creation"},
+    minable = {hardness = 0.2, mining_time = 0.1, result = "R-new-heat-exchanger-01"},
+    max_health = 200,
+    corpse = "medium-remnants",
+	crafting_speed = 1.00,
+    energy_source =
+    {
+      type = "burner",
+      effectivity = 1,
+      fuel_inventory_size = 1,
+      emissions = 0,
+    },
+    energy_usage = "5kW",
+    ingredient_count = 4,
+    crafting_categories = {"chemistry"},
+    resistances = 
+    {
+      {
+        type = "fire",
+        percent = 90
+      }
+    },
+    collision_box = {{-0.9, -0.9}, {0.9, 0.9}},
+    selection_box = {{-1, -1}, {1, 1}},
+    pictures =
+    {
+      filename = "__UraniumPower__/graphics/entity/new-heat-exchanger/heatexchanger1.png",
+      priority = "high",
+      width = 90,
+      height = 81,
+      axially_symmetrical = false,
+      direction_count = 4,
+      shift = {0.46, 0}
+    },
+    --[[animation =
+    {
+      filename = "__UraniumPower__/graphics/entity/new-heat-exchanger/heatexchanger1.png",
+      priority = "high",
+      width = 90,
+      height = 81,
+      frame_count = 1,
+      axially_symmetrical = false,
+      direction_count = 4,
+      shift = {0.46, 0}
+    },]]
+    animation =
+    {
+      north =
+      {
+        filename = "__UraniumPower__/graphics/entity/new-heat-exchanger/heatexchanger1.png",
+		width = 120,
+		height = 80,
+        frame_count = 1,
+		shift = {0.68, -0.05}
+      },
+      west =
+      {
+        filename = "__UraniumPower__/graphics/entity/new-heat-exchanger/heatexchanger1rot-270.png",
+		width = 120,
+		height = 80,
+        frame_count = 1,
+		shift = {0.68, -0.05}
+      },
+      south =
+      {
+        filename = "__UraniumPower__/graphics/entity/new-heat-exchanger/heatexchanger1-180.png",
+		width = 120,
+		height = 80,
+        frame_count = 1,
+		shift = {0.68, -0.05}
+      },
+      east =
+      {
+        filename = "__UraniumPower__/graphics/entity/new-heat-exchanger/heatexchanger1rot.png",
+		width = 120,
+		height = 80,
+        frame_count = 1,
+		shift = {0.68, -0.05}
+      }
+    },
+    working_sound =
+    {
+      sound = { filename = "__base__/sound/substation.ogg" },
+      apparent_volume = 1.5,
+    },
+    fluid_boxes =
+    {
+	   {
+        production_type = "input",
+        pipe_covers = pipecoverspictures(),
+		base_area = 7.5,
+        base_level = -1,
+        pipe_connections = {{ type="input", position = {0.5, 1.5} }}
+      },
+      {
+        production_type = "output",
+        pipe_covers = pipecoverspictures(),
+        base_area = 15,
+        base_level = 1,
+        pipe_connections = {{ type="output", position = {-0.5, 1.5} }} 
+      },
+      {
+        production_type = "input",
+        pipe_covers = pipecoverspictures(),
+        base_area = 15,
+        base_level = -1,
+        pipe_connections = {{ type="input", position = {-0.5, -1.5} }}
+      },
+      
+      {
+        production_type = "output",
+        pipe_covers = pipecoverspictures(),
+		base_area = 30,
+        base_level = 1,
+        pipe_connections = {{ type="output", position = {0.5, -1.5} }}
+      }
+    }
+  },
+  {
+    type = "item",
+    name = "R-new-heat-exchanger-01",
+    icon = "__UraniumPower__/graphics/entity/new-heat-exchanger/Rheatexchanger1icon.png",
+    flags = {"goes-to-quickbar"},
+	group = "uranium",
+    subgroup = "uranium-energy-pipe-distribution",
+    order = "a[items]-b[iron-chest]a",
+    place_result = "R-new-heat-exchanger-01",
+    stack_size = 50
+  },
+  {
+    type = "recipe",
+    name = "R-new-heat-exchanger-01",
+    enabled = "true",
+    energy_required = 0.1,
+    ingredients =
+    {
+      {"iron-plate", 1}
+    },
+    result = "R-new-heat-exchanger-01"
   },
   {
     type = "recipe",
