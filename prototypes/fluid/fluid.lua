add87c4bd47d81452e83067bf6227ab248ee87a5
Mcmuffin <rdirective@gmail.com>
2015-02-15 16:06:54 -0600

    New pressurized water icon
    +added icon png and edited fluid.lua

--- prototypes/fluid/fluid.lua
+++ prototypes/fluid/fluid.lua
@@ -9,7 +9,7 @@ data:extend(
 		heat_capacity = "1.5KJ",
 		base_color = {r=0, g=0.34, b=0.6},
 		flow_color = {r=0.7, g=0.7, b=0.7},
-		icon = "__base__/graphics/icons/fluid/water.png",
+		icon = "__UraniumPower__/graphics/icons/pressurized-water.png",
 		pressure_to_speed_ratio = 0.4,
 		flow_to_energy_ratio = 0.59,
 		order = "d"
