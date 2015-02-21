2e6d52609c15cebe9a69cec0e38080d4bfb0256e
Mcmuffin <rdirective@gmail.com>
2015-02-21 00:28:29 -0600

    Added "R" configuration of 2x2 pump

--- prototypes/entity/test-entities.lua
+++ prototypes/entity/test-entities.lua
@@ -1,10 +1,10 @@
 data:extend({
   {
     type = "pump",
-    name = "test-small-pump",
-    icon = "__UraniumPower__/graphics/entity/2x2pump/2x2pump.png",
+    name = "S-test-small-pump",
+    icon = "__UraniumPower__/graphics/entity/2x2pump/S2x2pump.png",
     flags = {"placeable-neutral", "player-creation"},
-    minable = {mining_time = 0.1, result = "test-small-pump"},
+    minable = {mining_time = 0.1, result = "S-test-small-pump"},
     max_health = 80,
     corpse = "small-remnants",
     resistances =
@@ -38,28 +38,28 @@ data:extend({
     {
       north =
       {
-        filename = "__UraniumPower__/graphics/entity/2x2pump/2x2pump.png",
+        filename = "__UraniumPower__/graphics/entity/2x2pump/S2x2pump.png",
         width = 64,
         height = 64,
         frame_count = 1
       },
       east =
       {
-        filename = "__UraniumPower__/graphics/entity/2x2pump/2x2pump.png",
+        filename = "__UraniumPower__/graphics/entity/2x2pump/S2x2pump.png",
         width = 64,
         height = 64,
         frame_count = 1
       },
       south =
       {
-        filename = "__UraniumPower__/graphics/entity/2x2pump/2x2pump.png",
+        filename = "__UraniumPower__/graphics/entity/2x2pump/S2x2pump.png",
         width = 64,
         height = 64,
         frame_count = 1
       },
       west =
       {
-        filename = "__UraniumPower__/graphics/entity/2x2pump/2x2pump.png",
+        filename = "__UraniumPower__/graphics/entity/2x2pump/S2x2pump.png",
         width = 64,
         height = 64,
         frame_count = 1
@@ -68,23 +68,111 @@ data:extend({
 	},
 	{
 		type = "item",
-		name = "test-small-pump",
-		icon = "__UraniumPower__/graphics/entity/2x2pump/2x2pump.png",
+		name = "S-test-small-pump",
+		icon = "__UraniumPower__/graphics/entity/2x2pump/S2x2pump.png",
 		flags = {"goes-to-quickbar"},
 		subgroup = "uranium-raw-resource",
 		order = "d[uraninite]",
 		stack_size = 50,
-		place_result = "test-small-pump",
+		place_result = "S-test-small-pump",
 	},
  	{
 		type = "recipe",
-		name = "test-small-pump",
+		name = "S-test-small-pump",
 		energy_required = 0.1,
 		enabled = "true",
 		ingredients =
 		{
 			{"iron-plate", 1}
 		},
-		result= "test-small-pump"
+		result= "S-test-small-pump"
+	},
+  {
+    type = "pump",
+    name = "R-test-small-pump",
+    icon = "__UraniumPower__/graphics/entity/2x2pump/R2x2pump.png",
+    flags = {"placeable-neutral", "player-creation"},
+    minable = {mining_time = 0.1, result = "R-test-small-pump"},
+    max_health = 80,
+    corpse = "small-remnants",
+    resistances =
+    {
+      {
+        type = "fire",
+        percent = 70
+      }
+    },
+    collision_box = {{-0.8, -0.8}, {0.8, 0.8}},
+    selection_box = {{-1.0, -1.0}, {1.0, 1.0}},
+    fluid_box =
+    {
+      base_area = 1,
+      pipe_covers = pipecoverspictures(),
+      pipe_connections =
+      {
+        { position = {-0.5, 1.5}, type="input" },
+        { position = {1.5, -0.5}, type="output" },
+      },
+    },
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "secondary-input",
+      emissions = 0.01 / 2.5
+    },
+    energy_usage = "500kW",
+    pumping_speed = 20.0,
+    animations =
+    {
+      north =
+      {
+        filename = "__UraniumPower__/graphics/entity/2x2pump/R2x2pump.png",
+        width = 64,
+        height = 64,
+        frame_count = 1
+      },
+      east =
+      {
+        filename = "__UraniumPower__/graphics/entity/2x2pump/R2x2pump.png",
+        width = 64,
+        height = 64,
+        frame_count = 1
+      },
+      south =
+      {
+        filename = "__UraniumPower__/graphics/entity/2x2pump/R2x2pump.png",
+        width = 64,
+        height = 64,
+        frame_count = 1
+      },
+      west =
+      {
+        filename = "__UraniumPower__/graphics/entity/2x2pump/R2x2pump.png",
+        width = 64,
+        height = 64,
+        frame_count = 1
+      }
+    }
+	},
+	{
+		type = "item",
+		name = "R-test-small-pump",
+		icon = "__UraniumPower__/graphics/entity/2x2pump/R2x2pump.png",
+		flags = {"goes-to-quickbar"},
+		subgroup = "uranium-raw-resource",
+		order = "d[uraninite]",
+		stack_size = 50,
+		place_result = "R-test-small-pump",
+	},
+ 	{
+		type = "recipe",
+		name = "R-test-small-pump",
+		energy_required = 0.1,
+		enabled = "true",
+		ingredients =
+		{
+			{"iron-plate", 1}
+		},
+		result= "R-test-small-pump"
 	},
 })
