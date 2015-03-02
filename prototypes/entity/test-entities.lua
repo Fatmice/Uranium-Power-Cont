17f576cf8617585c149ad4f057242fad1a9e5a34
Mcmuffin <rdirective@gmail.com>
2015-03-01 19:39:25 -0600

    Added three pipe prototypes
    -added test-pipe-a, -b, and -c

--- prototypes/entity/test-entities.lua
+++ prototypes/entity/test-entities.lua
@@ -338,4 +338,199 @@ data:extend({
 		},
 		result= "turbine-gen"
 	},
+  {
+    type = "pipe",
+    name = "test-pipe-A",
+    icon = "__UraniumPower__/graphics/entity/test-pipe/test-pipe-a.png",
+    flags = {"placeable-neutral", "player-creation"},
+    minable = {hardness = 0.2, mining_time = 0.5, result = "test-pipe-A"},
+    max_health = 50,
+    corpse = "small-remnants",
+    resistances =
+    {
+      {
+        type = "fire",
+        percent = 90
+      }
+    },
+    fast_replaceable_group = "pipe",
+    collision_box = {{-0.29, -0.29}, {0.29, 0.29}},
+    selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+    fluid_box =
+    {
+      base_area = 100,
+      pipe_connections =
+      {
+        { position = {0, -1} },
+        { position = {1, 0} },
+        { position = {0, 1} },
+        { position = {-1, 0} }
+      },
+    },
+    pictures = pipepictures(),
+    working_sound =
+    {
+      sound = {
+        {
+          filename = "__base__/sound/pipe.ogg",
+          volume = 0.65
+        },
+      },
+      match_volume_to_activity = true,
+      max_sounds_per_type = 3
+    },
+    horizontal_window_bounding_box = {{-0.25, -0.25}, {0.25, 0.15625}},
+    vertical_window_bounding_box = {{-0.28125, -0.40625}, {0.03125, 0.125}}
+  },
+	{
+		type = "item",
+		name = "test-pipe-A",
+		icon = "__UraniumPower__/graphics/entity/test-pipe/test-pipe-a.png",
+		flags = {"goes-to-quickbar"},
+		subgroup = "uranium-raw-resource",
+		order = "d[uraninite]",
+		stack_size = 50,
+		place_result = "test-pipe-A",
+	},
+ 	{
+		type = "recipe",
+		name = "test-pipe-A",
+		energy_required = 0.1,
+		enabled = "true",
+		ingredients =
+		{
+			{"iron-plate", 1}
+		},
+		result= "test-pipe-A"
+	},
+  {
+    type = "pipe",
+    name = "test-pipe-B",
+    icon = "__UraniumPower__/graphics/entity/test-pipe/test-pipe-b.png",
+    flags = {"placeable-neutral", "player-creation"},
+    minable = {hardness = 0.2, mining_time = 0.5, result = "test-pipe-B"},
+    max_health = 50,
+    corpse = "small-remnants",
+    resistances =
+    {
+      {
+        type = "fire",
+        percent = 90
+      }
+    },
+    fast_replaceable_group = "pipe",
+    collision_box = {{-0.29, -0.29}, {0.29, 0.29}},
+    selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+    fluid_box =
+    {
+      base_area = 100,
+      pipe_connections =
+      {
+        { position = {0, -1} },
+        { position = {1, 0} },
+        { position = {0, 1} },
+        { position = {-1, 0} }
+      },
+    },
+    pictures = pipepictures(),
+    working_sound =
+    {
+      sound = {
+        {
+          filename = "__base__/sound/pipe.ogg",
+          volume = 0.65
+        },
+      },
+      match_volume_to_activity = true,
+      max_sounds_per_type = 3
+    },
+    horizontal_window_bounding_box = {{-0.25, -0.25}, {0.25, 0.15625}},
+    vertical_window_bounding_box = {{-0.28125, -0.40625}, {0.03125, 0.125}}
+  },
+	{
+		type = "item",
+		name = "test-pipe-B",
+		icon = "__UraniumPower__/graphics/entity/test-pipe/test-pipe-b.png",
+		flags = {"goes-to-quickbar"},
+		subgroup = "uranium-raw-resource",
+		order = "d[uraninite]",
+		stack_size = 50,
+		place_result = "test-pipe-B",
+	},
+ 	{
+		type = "recipe",
+		name = "test-pipe-B",
+		energy_required = 0.1,
+		enabled = "true",
+		ingredients =
+		{
+			{"iron-plate", 1}
+		},
+		result= "test-pipe-B"
+	},
+  {
+    type = "pipe",
+    name = "test-pipe-C",
+    icon = "__UraniumPower__/graphics/entity/test-pipe/test-pipe-c.png",
+    flags = {"placeable-neutral", "player-creation"},
+    minable = {hardness = 0.2, mining_time = 0.5, result = "test-pipe-C"},
+    max_health = 50,
+    corpse = "small-remnants",
+    resistances =
+    {
+      {
+        type = "fire",
+        percent = 90
+      }
+    },
+    fast_replaceable_group = "pipe",
+    collision_box = {{-0.29, -0.29}, {0.29, 0.29}},
+    selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+    fluid_box =
+    {
+      base_area = 100,
+      pipe_connections =
+      {
+        { position = {0, -1} },
+        { position = {1, 0} },
+        { position = {0, 1} },
+        { position = {-1, 0} }
+      },
+    },
+    pictures = pipepictures(),
+    working_sound =
+    {
+      sound = {
+        {
+          filename = "__base__/sound/pipe.ogg",
+          volume = 0.65
+        },
+      },
+      match_volume_to_activity = true,
+      max_sounds_per_type = 3
+    },
+    horizontal_window_bounding_box = {{-0.25, -0.25}, {0.25, 0.15625}},
+    vertical_window_bounding_box = {{-0.28125, -0.40625}, {0.03125, 0.125}}
+  },
+	{
+		type = "item",
+		name = "test-pipe-C",
+		icon = "__UraniumPower__/graphics/entity/test-pipe/test-pipe-c.png",
+		flags = {"goes-to-quickbar"},
+		subgroup = "uranium-raw-resource",
+		order = "d[uraninite]",
+		stack_size = 50,
+		place_result = "test-pipe-C",
+	},
+ 	{
+		type = "recipe",
+		name = "test-pipe-C",
+		energy_required = 0.1,
+		enabled = "true",
+		ingredients =
+		{
+			{"iron-plate", 1}
+		},
+		result= "test-pipe-C"
+	},
 })
