b27707212b0dd0763cb9a6477bf2cac71cf72d28
McMuffin <rdirective@gmail.com>
2015-06-26 21:11:55 -0600

    Added fluid boxes for testing purposes
    -added 3 fluid boxes (feed water box, cold leg reservoir, low pressure
    steam reservoir) for testing purposes for the turbine generator
    -edited test-entities to reflect this

--- prototypes/entity/test-entities.lua
+++ prototypes/entity/test-entities.lua
@@ -928,5 +928,238 @@ data:extend(
 		},
 		result= "turbinegenpipe2"
 	},
+	
+	--turbine gen additional fluid boxes
+	--feed water box
+	
+	{
+		type = "storage-tank",
+		name = "turbine-generator-feed-water-box",
+		icon = "__UraniumPower__/graphics/entity/turbine-generator/feedwaterboxicon.png",
+		flags = {"placeable-neutral", "player-creation"},
+		minable = {hardness = 0.2, mining_time = 0.05},
+		max_health = 50,
+		corpse = "small-remnants",
+		resistances =
+		{
+			{
+				type = "fire",
+				percent = 90
+			}
+		},
+		collision_box = {{-1.29, -1.29}, {1.29, 1.29}},
+		selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+		fluid_box =
+		{
+			base_area = 10,
+			base_level = -1,
+			pipe_connections =
+			{
+				{ position = {0, -2} },
+				{ position = {2, 0} },
+				{ position = {0, 2} },
+				{ position = {-2, 0} }
+			},
+		},
+		picture =
+		{
+			sheet =
+			{
+			filename = "__UraniumPower__/graphics/entity/turbine-generator/feedwaterbox.png",
+			priority = "extra-high",
+			frames = 1,
+			width = 96,
+			height = 96,
+			}
+		},
+		working_sound =
+		{
+			sound =
+			{
+				{
+					filename = "__base__/sound/pipe.ogg",
+					volume = 0.65
+				},
+			},
+			match_volume_to_activity = true,
+			max_sounds_per_type = 3
+		},
+	},
+	{
+		type = "item",
+		name = "turbine-generator-feed-water-box",
+		icon = "__UraniumPower__/graphics/entity/turbine-generator/feedwaterboxicon.png",
+		flags = {"goes-to-quickbar"},
+		subgroup = "uranium-raw-resource",
+		order = "d[uraninite]",
+		stack_size = 50,
+		place_result = "turbine-generator-feed-water-box",
+	},
+ 	{
+		type = "recipe",
+		name = "turbine-generator-feed-water-box",
+		energy_required = 0.1,
+		enabled = "true",
+		ingredients =
+		{
+			{"iron-plate", 1}
+		},
+		result= "turbine-generator-feed-water-box"
+	},
 
+	--cold leg box
+	
+	{
+		type = "storage-tank",
+		name = "turbine-generator-cold-leg-box",
+		icon = "__UraniumPower__/graphics/entity/turbine-generator/coldlegicon.png",
+		flags = {"placeable-neutral", "player-creation"},
+		minable = {hardness = 0.2, mining_time = 0.05},
+		max_health = 50,
+		corpse = "small-remnants",
+		resistances =
+		{
+			{
+				type = "fire",
+				percent = 90
+			}
+		},
+		collision_box = {{-0.29, -0.29}, {0.29, 0.29}},
+		selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+		fluid_box =
+		{
+			base_area = 10,
+			base_level = -1,
+			pipe_connections =
+			{
+				{ position = {0, -1} },
+				{ position = {1, 0} },
+				{ position = {0, 1} },
+				{ position = {-1, 0} }
+			},
+		},
+		picture =
+		{
+			sheet =
+			{
+			filename = "__UraniumPower__/graphics/entity/turbine-generator/coldlegicon.png",
+			priority = "extra-high",
+			frames = 1,
+			width = 32,
+			height = 32,
+			}
+		},
+		working_sound =
+		{
+			sound =
+			{
+				{
+					filename = "__base__/sound/pipe.ogg",
+					volume = 0.65
+				},
+			},
+			match_volume_to_activity = true,
+			max_sounds_per_type = 3
+		},
+	},
+	{
+		type = "item",
+		name = "turbine-generator-cold-leg-box",
+		icon = "__UraniumPower__/graphics/entity/turbine-generator/coldlegicon.png",
+		flags = {"goes-to-quickbar"},
+		subgroup = "uranium-raw-resource",
+		order = "d[uraninite]",
+		stack_size = 50,
+		place_result = "turbine-generator-cold-leg-box",
+	},
+ 	{
+		type = "recipe",
+		name = "turbine-generator-cold-leg-box",
+		energy_required = 0.1,
+		enabled = "true",
+		ingredients =
+		{
+			{"iron-plate", 1}
+		},
+		result= "turbine-generator-cold-leg-box"
+	},
+	
+	
+	--low pressure steam reservoir box
+	
+	{
+		type = "storage-tank",
+		name = "turbine-generator-low-p-steam-box",
+		icon = "__UraniumPower__/graphics/entity/turbine-generator/steamreservoiricon.png",
+		flags = {"placeable-neutral", "player-creation"},
+		minable = {hardness = 0.2, mining_time = 0.05},
+		max_health = 50,
+		corpse = "small-remnants",
+		resistances =
+		{
+			{
+				type = "fire",
+				percent = 90
+			}
+		},
+		collision_box = {{-0.29, -0.29}, {0.29, 0.29}},
+		selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+		fluid_box =
+		{
+			base_area = 10,
+			base_level = -1,
+			pipe_connections =
+			{
+				{ position = {0, -1} },
+				{ position = {1, 0} },
+				{ position = {0, 1} },
+				{ position = {-1, 0} }
+			},
+		},
+		picture =
+		{
+			sheet =
+			{
+			filename = "__UraniumPower__/graphics/entity/turbine-generator/steamreservoiricon.png",
+			priority = "extra-high",
+			frames = 1,
+			width = 32,
+			height = 32,
+			}
+		},
+		working_sound =
+		{
+			sound =
+			{
+				{
+					filename = "__base__/sound/pipe.ogg",
+					volume = 0.65
+				},
+			},
+			match_volume_to_activity = true,
+			max_sounds_per_type = 3
+		},
+	},
+	{
+		type = "item",
+		name = "turbine-generator-low-p-steam-box",
+		icon = "__UraniumPower__/graphics/entity/turbine-generator/steamreservoiricon.png",
+		flags = {"goes-to-quickbar"},
+		subgroup = "uranium-raw-resource",
+		order = "d[uraninite]",
+		stack_size = 50,
+		place_result = "turbine-generator-low-p-steam-box",
+	},
+ 	{
+		type = "recipe",
+		name = "turbine-generator-low-p-steam-box",
+		energy_required = 0.1,
+		enabled = "true",
+		ingredients =
+		{
+			{"iron-plate", 1}
+		},
+		result= "turbine-generator-low-p-steam-box"
+	},
+	
 })
